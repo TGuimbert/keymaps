00c5ab27430c16e553222010037a6448319a1695
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2022-12-23 22:35:43 +0100

    Update to FR-GODOX

--- lulu/keymap.c
+++ lulu/keymap.c
@@ -14,7 +14,7 @@ enum layers {
 };
 
 #define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+#define LOWER TG(_LOWER)
 
 #define TG_O MT(MOD_LALT, BP_O)
 #define TG_U MT(MOD_LGUI, BP_U)
@@ -24,73 +24,88 @@ enum layers {
 #define TG_S MT(MOD_RCTL, BP_S)
 #define TG_N MT(MOD_RGUI, BP_N)
 #define TG_R MT(MOD_LALT, BP_R)
+#define TG_Q MT(MOD_RALT, BP_Q)
+#define TG_C MT(MOD_RALT, BP_C)
+#define TG_COMM LT(_LOWER, BP_COMM)
+#define TG_QUOT LT(_LOWER, BP_QUOT)
+#define TG_E LT(_RAISE, BP_E)
+
+
+#define TG_LDAQ MT(MOD_LALT, BP_LDAQ)
+#define TG_LEFT MT(MOD_LGUI, KC_LEFT)
+#define TG_DOWN MT(MOD_LCTL, KC_DOWN)
+#define TG_RGHT MT(MOD_LSFT, KC_RGHT)
+#define TG_P4 MT(MOD_RSFT, KC_P4)
+#define TG_P5 MT(MOD_RCTL, KC_P5)
+#define TG_P6 MT(MOD_RGUI, KC_P6)
+#define TG_SLSH MT(MOD_LALT, BP_SLSH)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
-/* QWERTY
+/* FR-GODOX
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * | ESC  |   $  |   &  |   (  |   )  |      |                    |      |   @  |   "  |   =  |   %  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   À  |   B  |   É  |   .  |   -  |                    |   ^  |   V  |   L  |   M  |   X  |  Ç   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |Enter |   O  |   U  |   A  |   I  |   J  |-------.    ,-------|   G  |   T  |   S  |   N  |   R  |  F   |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LShift|   Q  |   Y  |   È  |   P  |   K  |-------|    |-------|   W  |   D  |   ,  |   H  |   C  |  Z   |
+ * | ESC  |   O  |   U  |   A  |   I  |   J  |-------.    ,-------|   G  |   T  |   S  |   N  |   R  |  F   |
+ * |------+------+------+------+------+------|Play/|||    |  BOOT |------+------+------+------+------+------|
+ * |LOWER |   Q  |   Y  |   È  |   P  |   K  |-------|    |-------|   W  |   D  |   Ê  |   H  |   C  |  Z   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI |LOWER |   E  | /   ,   /       \  '   \  |Space |BackSP|RAISE |
- *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   | LGUI |ENTER |   E  | /   ,   /       \  '   \  |Space |BackSP|Delete|
+ *                   |      |      | RAISE|/ LOWER /         \ LOWER\ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
- [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB, BP_AGRV,  BP_B,    BP_EACU, BP_DOT, BP_MINS,                BP_DCIR,    BP_V,    BP_L,    BP_M,    BP_X,    BP_CCED,
-  KC_ENT ,  TG_O,   TG_U,    TG_A,    TG_I,    BP_J,                     BP_G,    TG_T,    TG_S,    TG_N,    TG_R,    BP_F,
-  KC_LSFT,  BP_Q,   BP_Y,  BP_EGRV,   BP_P,    BP_K, KC_LBRC,  QK_BOOT,  BP_W,    BP_D,    KC_COMM, BP_H,    BP_C,    BP_Z,
-                           KC_LGUI,  LOWER,    BP_E, BP_COMM,  BP_QUOT, KC_SPC, KC_BSPC,   RAISE
+[_QWERTY] = LAYOUT(
+   KC_ESC,  BP_DLR, BP_AMPR, BP_LPRN, BP_RPRN, XXXXXXX,                   XXXXXXX,   BP_AT, BP_DQUO,  BP_EQL, BP_PERC, XXXXXXX,
+   KC_TAB, BP_AGRV,    BP_B, BP_EACU,  BP_DOT, BP_MINS,                   BP_DCIR,    BP_V,    BP_L,    BP_M,    BP_X, BP_CCED,
+   KC_ESC,    TG_O,    TG_U,    TG_A,    TG_I,    BP_J,                      BP_G,    TG_T,    TG_S,    TG_N,    TG_R,    BP_F,
+    LOWER,    TG_Q,    BP_Y, BP_EGRV,    BP_P,    BP_K, KC_MPLY, QK_BOOT,    BP_W,    BP_D, BP_ECIR,    BP_H,    TG_C,    BP_Z,
+                             KC_LGUI,  KC_ENT,    TG_E, TG_COMM, TG_QUOT,  KC_SPC, KC_BSPC,  KC_DEL
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |      |   {  |   }  |  UP  |   (  |   )  |                    |   -  |   7  |   8  |   9  |   *  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
+ * |      |   «  | LEFT | DOWN | RIGHT|   »  |-------.    ,-------|   =  |   4  |   5  |   6  |   /  |   \  |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |   [  |   ]  |   <  |   >  |      |-------|    |-------|      |   1  |   2  |   3  |   +  |   %  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      | /       /       \      \  |  0   |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-  _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
+  _______, BP_LCBR, BP_RCBR,   KC_UP, BP_LPRN, BP_RPRN,                   BP_MINS,   KC_P7,   KC_P8,   KC_P9, BP_ASTR, _______,
+  _______, TG_LDAQ, TG_LEFT, TG_DOWN, TG_RGHT, BP_RDAQ,                    BP_EQL,   TG_P4,   TG_P5,   TG_P6, TG_SLSH, BP_BSLS,
+  _______, BP_LBRC, BP_RBRC, BP_LABK, BP_RABK, _______, _______, _______, XXXXXXX,   KC_P1,   KC_P2,   KC_P3, BP_PLUS, BP_PERC,
+                             _______, _______, _______, _______, _______,   KC_P0, _______, _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |MOD   | MODR |  S+  |  S-  |  B+  |  B-  |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      | Left | Down |  Up  |Right |      |
- * |------+------+------+------+------+------|RGB_TOG|    |    ]  |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
+ * |------+------+------+------+------+------|RGB_TOG|    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |      |      |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_RAISE] = LAYOUT(
-  RGB_MOD, RGB_RMOD,RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,                     _______, _______, _______, _______, _______, _______,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   RGB_TOG, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
+  RGB_MOD, RGB_RMOD,RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,                   _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______,
+                             _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
